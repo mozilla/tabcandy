a18da6b7ea5dbb38b41761d79d546fb51550c867
Ian Gilman <ian@iangilman.com>
2010-07-30 14:53:35 -0700

    + updated doc script for new file location + updated docs

--- tcc-docs.sh
+++ tcc-docs.sh
@@ -1,1 +1,1 @@
-naturaldocs/NaturalDocs -i ../tabcandy-central/browser/base/content/tabcandy/ -o HTML content/doc -p naturaldocs-data -r
+naturaldocs/NaturalDocs -i ../tabcandy-central/browser/base/content/tabview/ -o HTML content/doc -p naturaldocs-data -r
